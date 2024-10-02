91fabbe253cf62a9cd3e48dcbdf9da62211ce577
YasminMuntaserN <161508263+YasminMuntaserN@users.noreply.github.com>
2024-10-02 12:08:47 +0300

    feat: add  tailwind style into Header

--- src/features/ui/Header.jsx
+++ src/features/ui/Header.jsx
@@ -4,7 +4,7 @@ import UserName from "../user/UserName"
 
 function Header() {
   return (
-    <header className="bg-yellow-500 px-4 py-3 uppercase border-b border-stone-200">
+    <header className="flex items-center justify-between bg-yellow-500 px-4 py-3 uppercase border-b border-stone-200 sm:px-6">
       <Link to="/" className="tracking-widest">Fast React Pizza Co.</Link>
       <SearchOrder/>
       <UserName/>
