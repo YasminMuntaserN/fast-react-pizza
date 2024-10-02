6669779fe2d8ea0cf5fcd98720bfd6bdbae3f475
YasminMuntaserN <161508263+YasminMuntaserN@users.noreply.github.com>
2024-10-02 17:37:59 +0300

    Styling the Menu

--- src/features/ui/Button.jsx
+++ src/features/ui/Button.jsx
@@ -1,15 +1,22 @@
 import { Link } from "react-router-dom";
 
-function Button({children ,disabled  ,to}) {
-  const className ="bg-yellow-400 uppercase font-semibold text-stone-800 py-3 px-4 inline-block tracking-wide rounded-full hover:bg-yellow-300 transition-colors duration-300 focus:outline-none focus:ring focus:bg-ring-yellow-300 focus:ring-yellow-300 focus:ring-offset-2 disabled:cursor-not-allowed sm:py-4 sm:px-6";
+function Button({children ,disabled  ,to , type}) {
+
+  const base ="bg-yellow-400 uppercase font-semibold text-stone-800 inline-block tracking-wide rounded-full hover:bg-yellow-300 transition-colors duration-300 focus:outline-none focus:ring focus:bg-ring-yellow-300 focus:ring-yellow-300 focus:ring-offset-2 disabled:cursor-not-allowed ";
+
+  const styles ={
+    primary:base+'  py-3 px-4  md:py-6 md:px-4',
+    small:base +" py-2 px-4 md:py-5 md:px-2.5 text-xs",
+    secondary:" border-2 border-stone-300 uppercase font-semibold text-stone-400 inline-block tracking-wide rounded-full hover:bg-stone-300 transition-colors duration-300 focus:outline-none focus:ring focus:bg-ring-stone-300 focus:ring-stone-300 focus:ring-offset-2 disabled:cursor-not-allowed  py-3 px-4  md:py-6 md:px-4 "
+  };
 
   if(to)
     return (
-  <Link to={to} className={className}>{children}</Link>
+  <Link to={to} className={styles[type]}>{children}</Link>
     );
 
   return (
-    <button disabled={disabled} className={className}>
+    <button disabled={disabled} className={styles[type]}>
       {children}
     </button>
   )
