c79c9ff1628721139a8faec65b8df32708ea0057
YasminMuntaserN <161508263+YasminMuntaserN@users.noreply.github.com>
2024-10-04 13:12:15 +0300

    Updating Cart Quantities

--- src/features/menu/MenuItem.jsx
+++ src/features/menu/MenuItem.jsx
@@ -1,12 +1,15 @@
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import Button from "../ui/Button";
 import { formatCurrency } from "../utilts/helpers";
-import { addItem } from "../cart/cartSlice";
+import { addItem, getCurrentQuantityById } from "../cart/cartSlice";
+import DeleteItem from "../cart/DeleteItem";
 
 function MenuItem({ pizza }) {
   const dispatch =useDispatch();
-  const {id,  name, unitPrice, ingredients, soldOut, imageUrl } = pizza;
 
+  const {id,  name, unitPrice, ingredients, soldOut, imageUrl } = pizza;
+  const currentQuantity =useSelector(getCurrentQuantityById(id));
+  const IsInCart =currentQuantity > 0;
   function handleAddToCart(){
     const newItem = 
       {
@@ -27,9 +30,10 @@ function MenuItem({ pizza }) {
         <p className="font-sm italic text-stone-500 capitalize">{ingredients.join(', ')}</p>
         <div className="mt-auto flex  items-center justify-between">
           {!soldOut ? <p className="text-sm">{formatCurrency(unitPrice)}</p> : <p className="text-sm uppercase text-stone-500" >Sold out</p>}
+        
+        { IsInCart && <DeleteItem pizzaId={id}/>}
 
-
-          {!soldOut && <Button type="small" onClick={handleAddToCart}>Add to cart</Button>}
+          {!soldOut && !IsInCart && <Button type="small" onClick={handleAddToCart}>Add to cart</Button>}
         </div>
       </div>
     </li>
